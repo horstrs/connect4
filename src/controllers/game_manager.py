d790c5b82a149cb78a255822f7ea89b920e8a66c
horstrs <horst.marcelo@gmail.com>
2026-01-29 16:01:40 -0300

    Finished basic CLI functionality

--- src/controllers/game_manager.py
+++ src/controllers/game_manager.py
@@ -16,18 +16,19 @@ class GameManager:
         game_ended = False
         next_move = self._get_move(current_player)
         while not self.game_board.is_column_playable(next_move):
-            print("invalid move")  # TODO: Implement print in view class
+            self.view.print_error_message("invalid move")
+            self.view.print_board(self.game_board.board_state)
+
             next_move = self._get_move(current_player)
 
         board_coord = self.game_board.add_piece(next_move, current_player.id)
         if self.game_board.has_player_won(board_coord):
-            print(
-                f"Game Over! {current_player.name} has won"
-            )  # TODO: Move this to View class
+            self.view.print_game_over_screen(f"Game Over! {current_player.name} has won", current_player.id)
+            
             game_ended = True
             return game_ended
         if self.game_board.is_tie():
-            print("Game Over! It's a tie!")  # TODO: Move this to View class
+            self.view.print_game_over_screen("Game Over! It's a tie!")
             game_ended = True
             return game_ended
 
@@ -35,7 +36,7 @@ class GameManager:
 
     def _get_move(self, current_player: Player):
         if current_player.is_human():
-            return self.view.get_user_input()
+            return self.view.get_user_input(current_player.name)
         else:
             return current_player.get_move(self.game_board)
 
--- src/views/cli_view.py
+++ src/views/cli_view.py
@@ -4,25 +4,41 @@ from src.config import PLAYERS_CONFIG, CELL_DIVIDER, BOLD, RESET, COLOR_MAPPING
 
 class CLIView:
     def __init__(self):
-        self.name = ""
+        self.player_config = self._load_player_config()
+
+    def _load_player_config(self) -> dict:
+        player_config = PLAYERS_CONFIG.get(Cell.PLAYER1)
+        if not player_config or player_config is None:
+            raise AttributeError(f"Missing configuration for player {Cell.PLAYER1}")
+
+        player_config = PLAYERS_CONFIG.get(Cell.PLAYER2)
+        if not player_config or player_config is None:
+            raise AttributeError(f"Missing configuration for player {Cell.PLAYER2}")
+
+        return PLAYERS_CONFIG
 
     def _convert_cell_data_to_player_config(self, cell_data: int) -> str:
         if cell_data == Cell.EMPTY.value:
             return "   "
 
-        player_config = PLAYERS_CONFIG.get(cell_data)
-        if not player_config or player_config is None:
-            raise AttributeError(f"Missing configuration for player {cell_data}")
-
-        color_name = player_config.get("COLOR")
+        current_player_config = self.player_config.get(cell_data)
+        color_name = current_player_config.get("COLOR")
         ansi_code = COLOR_MAPPING.get(color_name, "")
-        shape = player_config.get("SHAPE", " ").upper()
+        shape = current_player_config.get("SHAPE", " ").upper()
 
         if shape.strip():
             return f"{BOLD}{ansi_code} {shape} {RESET}"
-        
+
         bg_ansi = COLOR_MAPPING.get(f"bg_{color_name}", ansi_code)
         return f"{bg_ansi}   {RESET}"
+
+    def _get_player_ansi_color(self, player_id: int) -> str:
+        if player_id == Cell.EMPTY.value:
+            return ""
+        current_player_config = self.player_config.get(player_id)
+        color_name = current_player_config.get("COLOR")
+        ansi_code = COLOR_MAPPING.get(color_name, "")
+        return ansi_code
 
     def print_board(self, board_state: list[list[Cell]]) -> str:
         rows = []
@@ -39,6 +55,18 @@ class CLIView:
         board = "\n".join(rows) + "\n" + floor
         print("\n" + head_foot + "\n" + board + "\n" + head_foot)
 
-    def get_user_input(self):  # TODO: Change implementation to get input from user
-        print("Select a column to play:")
-        pass
+    def get_user_input(self, player_name: str) -> int:
+        print(f"\n{player_name}'s turn.")
+        val = input("Select a column to play:")
+        try:
+            return int(val)
+        except ValueError:
+            return -1
+
+    def print_error_message(self, message: str) -> None:
+        red = COLOR_MAPPING.get("red", "")
+        print(f"{BOLD}{red}>> {message}{RESET}")
+
+    def print_game_over_screen(self, message: str, player_id: int = 0) -> None:
+        ansi_color = self._get_player_ansi_color(player_id)
+        print(f"{ansi_color}{message}{RESET}")
